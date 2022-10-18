10ec7be238e3b20b4171e1f255f188a6256ec7bf
Tristin <tmille2004@gmail.com>
2022-10-17 22:43:44 -0500

    adds pomodoro timer

--- frontend/src/components/Pomodoro.js
+++ frontend/src/components/Pomodoro.js
@@ -7,16 +7,20 @@ const Pomodoro = ({getCookie}) => {
     const [workLength, setWorkLength] = useState('')
     const [breakLength, setBreakLength] = useState('')
     const [show, setShow] = useState(false);
+    const [workState, setWorkState] = useState('work')
+    const [timer, setTimer] = useState('')
+    const [isTimer, setIsTimer] = useState(false)
 
     useEffect(() => {
         const getPomodoroSettings = async () => {
             const pomodoroSettings = await fetchPomodoroSettings()
             setWorkLength(pomodoroSettings.work_length)
+            setTimer(pomodoroSettings.work_length + ":00")
             setBreakLength(pomodoroSettings.break_length)
         }
 
         getPomodoroSettings()
-    }, [])
+    }, [workLength])
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
@@ -58,16 +62,53 @@ const Pomodoro = ({getCookie}) => {
         return `${minutes}:${seconds}`
     }
 
+    function pomodoro() {
+        if (workState == 'work' && workLength != '') {
+            setIsTimer(true)
+            let secondsLeft = workLength*60
+            startWork(secondsLeft)
+        } else if (workState == 'break' && breakLength != '0') {
+            setIsTimer(true)
+            let secondsLeft = breakLength*60
+            startBreak(secondsLeft)
+        }
+    }
+
+    const startWork = (secondsLeft) => {
+        let loop = setInterval(() => {
+            secondsLeft -= 1
+            setTimer(calcMinsSecs(secondsLeft))
+            if (secondsLeft == 0) {
+                setTimer(calcMinsSecs(breakLength*60))
+                setWorkState('break')
+                setIsTimer(false)
+                clearInterval(loop)
+            }
+        }, 1000)
+    }
+
+    const startBreak = (secondsLeft) => {
+        let loop = setInterval(() => {
+            secondsLeft -= 1
+            setTimer(calcMinsSecs(secondsLeft))
+            if (secondsLeft == 0) {
+                setTimer(calcMinsSecs(workLength*60))
+                setWorkState('work')
+                setIsTimer(false)
+                clearInterval(loop)
+            }
+        }, 1000)
+    }
     
 
     return (
         <>
-            <div>
-                <p className='time-left'></p>
+            <div className="pomodoro">
+                <p className="work-state">{workState}</p>
+                <p className='time-left'>{timer}</p>
             </div>
-            {/* have this button pop up a modal for the settings */}
-            <Button variant="primary" name="pomodoro-start">Start</Button>
-            <Button variant="primary" name="pomodoro-start">Pause</Button>
+            {isTimer == false ? <Button variant="primary" name="pomodoro-start" onClick={pomodoro}>Start</Button> : 
+                            <Button variant="primary" name="pomodoro-start">Pause</Button>}
 
             <Button variant="primary" name="pomodoro-settings" onClick={handleShow}>Settings</Button>
 
